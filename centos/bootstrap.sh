c9e8b23b79bf308043b0ec1aad0206956ac86cb2
Kubik <amigostarist@gmail.com>
2022-08-31 22:51:06 +0300

    install php

--- centos/bootstrap.sh
+++ centos/bootstrap.sh
@@ -1,14 +1,22 @@
 #!/usr/bin/env bash
 
-yum update
-yum install epel-release
-yum install -y nginx
-systemctl start nginx
-systemctl enable nginx
-firewall-cmd --zone=public --permanent --add-service=http
-firewall-cmd --zone=public --permanent --add-service=https
-firewall-cmd --reload
-if ! [ -L /var/www ]; then
-  rm -rf /var/www
-  ln -fs /centos /var/www
-fi
+sudo yum update
+sudo yum install epel-release
+sudo yum install -y nginx
+sudo systemctl start nginx
+sudo systemctl enable nginx
+#firewall-cmd --zone=public --permanent --add-service=http
+#firewall-cmd --zone=public --permanent --add-service=https
+#firewall-cmd --reload
+#if ! [ -L /var/www ]; then
+ # rm -rf /var/www
+#  ln -fs /centos /var/www
+#fi
+#sudo mkdir  /var/www/php-test
+#sudo chown vagrant:vagrant /var/www/php-test/
+sudo cp -r ~/admin/ /usr/share/nginx/html/
+sudo mkdir -p /usr/share/nginx/html/php-test
+sudo chown vagrant:vagrant  /usr/share/nginx/html/php-test
+sudo echo "<?php phpinfo(); ?>" > php-test.php  /usr/share/nginx/html/php-test/
+sudo yum install -y php-fpm
+sudo systemctl start php-fpm
